f2835309096bcc9a277a5ea7628fd088833765c4
ying <ying.geng5@gmail.com>
2021-02-07 18:15:11 -0500

    fixed plot function typo

--- distributions/Gaussiandistribution.py
+++ distributions/Gaussiandistribution.py
@@ -105,6 +105,7 @@ class Gaussian(Distribution):
         plt.title("Histogram of Data")
         plt.xlabel('data')
         plt.ylabel('count')
+	plt.show()
 
     def pdf(self,x):
         """Probability density function calulator for the Gaussian distribution.
